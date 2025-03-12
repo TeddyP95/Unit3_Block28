ab5b54f7898f001d95321adba39d430926ccca64
Teddy Pillay <tpillay1995@gmail.com>
2025-03-11 21:14:05 -0400

    created Red, Blue and Home pages

--- ReactRouter/src/App.jsx
+++ ReactRouter/src/App.jsx
@@ -1,16 +1,23 @@
 import { useState } from 'react'
-import { Routes, Route } from "react-router-dom"
+import { Routes, Route, Link } from "react-router-dom"
+import Red from './Components/Red'
+import Blue from './Components/Blue'
+import Home from './Components/Home'
 
 function App() {
   return (
     <div id="container">
       <h1></h1>
-      <div id="navbar">{/* navigation here */}</div>
+      <div id="navbar">
+        <Link to="/">home</Link>
+        <Link to="/blue">blue</Link>
+        <Link to="/red">red</Link>
+      </div>
       <div id="main-section">
         <Routes>
-          <Route path="/blue" element={<h1>Blue</h1>} />
-          <Route path="/red" element={<h1>Red</h1>} />
-         
+          <Route path="/" element={<Home />} />
+          <Route path="/blue" element={<Blue />} />
+          <Route path="/red" element={<Red />} />
         </Routes>
       </div>
     </div>
